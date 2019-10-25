b0d4a1caec65995d516d73c8e3229926a7080869
Dominik Heller <github@dheller.de>
2019-10-25 20:36:39 +0200

    Added animated Map Pins

---
+++ Assets/Scripts/MapPinAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPinAnimation : MonoBehaviour
+{
+    public float spinForce;
+    public float temp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, temp * Time.deltaTime, 0);
+    }
+
+    public void StartSpinning()
+    {
+        temp = spinForce;
+      
+    }
+
+    public void StopSpinning()
+    {
+       temp = 0;
+
+    }
+}
