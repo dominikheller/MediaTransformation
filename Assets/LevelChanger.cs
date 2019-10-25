4c92cbcc48e5366914cf80e3c7a29940068db9ec
Maxdom <github@dheller.de>
2019-10-25 15:35:58 +0200

    Initial Commit
    0.1 Proof-of-Concept Project with Placeholders
    
    Featues:
    -Trigger by click
    -Trigger while looking
    -Play audio by trigger
    -Change scene by trigger
    -Scene transition (broken on phone?)
    -Baked emissive lighting
    -PostFX like Bloom, Anti-Aliasing and Ambient Occlusion
    -Low Poly Particle effects for fire
    -Runs on gear VR with TouchPad, asynchronous time warp etc.

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private string levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+
+    public void FadeToLevel(string levelName)
+    {
+        levelToLoad = levelName;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete() {
+
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+}
