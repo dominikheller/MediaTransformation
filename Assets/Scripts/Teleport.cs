12f04e9a0c0f95ea1fd029c910c89ab58dded85f
Dominik Heller <dheller@smail.uni-koeln.de>
2019-10-26 21:48:21 +0200

    Added teleportation points

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public void TeleportPlayer()
+    {
+        player.transform.position = new Vector3(transform.position.x, transform.position.y + 1.3f, transform.position.z);
+    }
+
+}
