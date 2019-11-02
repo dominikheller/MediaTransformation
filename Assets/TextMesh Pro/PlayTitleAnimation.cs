afd9a140f919a6170a732d963f050923a64fb572
Dominik Heller <dheller@smail.uni-koeln.de>
2019-11-02 23:38:28 +0100

    Added Title with Animations
    Just a proof of concept

---
+++ Assets/TextMesh Pro/PlayTitleAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTitleAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+
+    public void NextAnim() {
+
+        myAnimationController.SetInteger("animInt", myAnimationController.GetInteger("animInt") + 1);
+    
+    }
+}
