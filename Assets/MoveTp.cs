6774c33a222d7084063ba37673eb02c878242ac2
MoritzMeid <moritzmeid@yahoo.de>
2019-11-12 23:20:32 +0100

    VLight Add
    Neues Plugin + atmolicht in Hub1

---
+++ Assets/MoveTp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTp : MonoBehaviour
+{
+    Vector3 newPosition = new Vector3(0.0f, 983.06f, 0.0f);
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/Hub0/MoveTeleports.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTeleports : MonoBehaviour
+{
+
+    public GameObject teleport;
+    
+    public void TeleportTeleport()
+    {
+
+        teleport.transform.position = new Vector3(transform.position.x, transform.position.y + 983.06f, transform.position.z);
+    }
+}
