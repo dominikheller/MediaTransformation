cbecdd3b0be36232ed5e534aafb5ecd570bb4fbd
Dominik Heller <dheller@smail.uni-koeln.de>
2019-11-17 20:48:43 +0100

    added chimney to barn1

---
+++ Assets/Scripts/TriggerEventCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEventCounter : MonoBehaviour
+{
+    public LevelChanger levelChanger;
+
+    public int triggerCount = 0;
+
+    private int maxTriggerCountValue = 5;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        triggerCount++;
+
+        if (triggerCount == maxTriggerCountValue)
+        {
+            Debug.Log("Trigger something!");
+
+            // for example the level changer
+            // levelChanger.FadeToLevel("levelName");
+
+            triggerCount = 0;
+        }
+    }
+}
