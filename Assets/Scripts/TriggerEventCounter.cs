ace3875f51424ef2313503fe12a95a970f4bd059
malbero <34505168+malbero@users.noreply.github.com>
2019-12-01 21:29:38 +0100

    Finalize trigger event counter script

--- Assets/Scripts/TriggerEventCounter.cs
+++ Assets/Scripts/TriggerEventCounter.cs
@@ -5,37 +5,90 @@ using UnityEngine.UI;
 
 public class TriggerEventCounter : MonoBehaviour
 {
+    /**
+     * Canvas with score.
+     **/
     public Text Score;
 
-    public int triggerCount = 0;
+    /**
+     * Current found clues.
+     **/
+    private int triggerCount = 0;
 
-    private int maxTriggerCountValue = 2;
+    /**
+     * Maximum clue count.
+     **/
+    private int maxTriggerCountValue = 3;
+
+    /**
+     * Clue list (is needed to check if the current raycasted clue was already triggered).
+     **/
+    private List<string> clueList;
 
 
+    /**
+     * Inits variables and functions.
+     **/
     private void Start()
     {
+        clueList = new List<string>();
         setScore();
     }
 
 
+    /**
+     * Increases trigger count.
+     **/
     public void onPointerClick()
     {
-        triggerCount++;
-        setScore();
+        bool didHitAndNotTriggeredYet = validateRaycastedObject();
 
-        if (triggerCount == maxTriggerCountValue)
-        {
-            Debug.Log("Trigger something!");
+        if (didHitAndNotTriggeredYet == true) {
 
-            //@todo for example set win text + load new scene
+            triggerCount++;
+            setScore();
 
-            triggerCount = 0;
+            if (triggerCount == maxTriggerCountValue)
+            {
+                Debug.Log("Trigger something!");
+
+                //@todo for example set win text + load new scene
+          
+                triggerCount = 0;
+            }
         }
     }
 
 
+    /**
+     * Sets score.
+     **/
     private void setScore()
     {
         Score.text = triggerCount.ToString() + "/" + maxTriggerCountValue.ToString();
+    }
+
+    /**
+     * Validates if raycasted object was already triggered.
+     **/
+    private bool validateRaycastedObject()
+    {
+        // raycast to screen center
+        Ray toCenter = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit rhInfo;
+        bool didHit = Physics.Raycast(toCenter, out rhInfo, 500.0f);
+
+        if (didHit)
+        {
+            if (clueList.Contains(rhInfo.collider.transform.parent.name))
+            {
+                return false;
+            }
+
+            clueList.Add(rhInfo.collider.transform.parent.name);
+            return true;
+        }
+
+        return false;
     }
 }
