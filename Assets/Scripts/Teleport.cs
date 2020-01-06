f4bd7a391148904f874a8f911f4ce9a042374790
malbero <34505168+malbero@users.noreply.github.com>
2020-01-06 10:21:35 +0100

    Fix teleport script

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -9,7 +9,7 @@ public class Teleport : MonoBehaviour
 
     public void TeleportPlayer()
     {
-        player.transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+        player.transform.position = new Vector3(transform.position.x, transform.position.y + 1.3f, transform.position.z);
     }
 
 }
