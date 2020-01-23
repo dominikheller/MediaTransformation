1963e63a5eec59a6bde845ab46c97aaa9f1eb8e0
malbero <34505168+malbero@users.noreply.github.com>
2020-01-23 15:23:02 +0100

    Finalize clue trigger for barn1

---
+++ Assets/Scripts/ColliderRemover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderRemover : MonoBehaviour
+{
+    public void removeChildrenColliders()
+    {
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
+
